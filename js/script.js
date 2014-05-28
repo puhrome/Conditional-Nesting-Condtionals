1f927775c74d5eae4dc3208780c47bb63e70cf27
puhrome <phirom.yim@fullsail.edu>
2014-05-28 11:36:11 -0400

    Nesting Condtionals
    Learning how to nest a conditional within a conditional in WebStorm

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Phirom Yim on 5/14/14.
+ */
+//Nesting Condtionals
+
+if(sunny){
+    goToTheBeach();
+
+    if(warmWater){
+        wearNewSuit();
+    }
+}else{
+    goToTheMovies();
+}
